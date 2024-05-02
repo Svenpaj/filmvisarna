e3da0b2b256c19cbe126e3dbb8473bc008102889
Svenpaj <114157351+Svenpaj@users.noreply.github.com>
2024-05-02 23:35:34 +0200

    fixed backed bug

--- index.js
+++ index.js
@@ -1,4 +1,9 @@
 import express from 'express';
+import { fileURLToPath } from 'url';
+import path from 'path';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 // Port to run on
 const port = 5000;
@@ -10,12 +15,16 @@ const port = 5000;
 // Create a web server
 const app = express();
 
+app.use(express.json());
+
 // Serve all the files in the dist folder
 app.use(express.static('./dist'));
 
+// I need to make sure that index.html is served for all routes
+
 // Always serve index.html if nothing else found
 app.get('*', (req, res) => {
-  res.sendFile('./dist', './index.html');
+  res.sendFile(path.resolve(__dirname, 'dist', 'index.html'));
 });
 
 // Start the web server on a port
