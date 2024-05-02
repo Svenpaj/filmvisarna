46dd60de5630816d01e793baa197f1154c9b9c0c
Svenpaj <114157351+Svenpaj@users.noreply.github.com>
2024-05-02 21:38:20 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,22 +1,23 @@
+import express from 'express';
+
 // Port to run on
 const port = 5000;
 
 // Import express for creating a web server
-const express = require('express');
 
 // Import path to handle file paths
-const path = require('path');
 
 // Create a web server
 const app = express();
 
 // Serve all the files in the dist folder
-app.use(express.static('dist'));
+app.use(express.static('./dist'));
 
 // Always serve index.html if nothing else found
 app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, 'dist', 'index.html'));
+  res.sendFile('./dist', './index.html');
 });
 
 // Start the web server on a port
 app.listen(port, () => console.log('Listening on port ' + port));
+
