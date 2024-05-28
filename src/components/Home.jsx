47e2ed923856105d16debfc0dec4dda951590b29
Svenpaj <114157351+Svenpaj@users.noreply.github.com>
2024-05-28 13:45:22 +0200

    new headline on start page

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -77,7 +77,7 @@ export default function Home() {
       <div className="featuredMovieCTA-container">
         <div className="featuredMovieCTA-image">
           <a href="/movie/babylon" style={{ textDecoration: "none" }}>
-            <h1> Hej jag är en ändring </h1>
+            <h1> Linus är detta ok? </h1>
           </a>
         </div>
       </div>
