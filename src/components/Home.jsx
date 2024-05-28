fe8cff0e0b67793d4c72255d82c4f5be78d5668b
Svenpaj <114157351+Svenpaj@users.noreply.github.com>
2024-05-28 15:23:22 +0200

    changed h1 in home again

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -77,7 +77,7 @@ export default function Home() {
       <div className="featuredMovieCTA-container">
         <div className="featuredMovieCTA-image">
           <a href="/movie/babylon" style={{ textDecoration: "none" }}>
-            <h1> Linus är detta ok? och mer text som inte fanns innan </h1>
+            <h1> Babylon </h1>
           </a>
         </div>
       </div>
