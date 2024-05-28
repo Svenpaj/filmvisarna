b7502f7421c645b9df33506bf3b55eb71d4b23e7
Svenpaj <114157351+Svenpaj@users.noreply.github.com>
2024-05-28 12:19:34 +0200

    made a change in h1 home

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -77,7 +77,7 @@ export default function Home() {
       <div className="featuredMovieCTA-container">
         <div className="featuredMovieCTA-image">
           <a href="/movie/babylon" style={{ textDecoration: "none" }}>
-            <h1> Babylon </h1>
+            <h1> Hej jag är en ändring </h1>
           </a>
         </div>
       </div>
